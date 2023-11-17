410bacee726effd1759fd24d586a6d4c27e6ae64
polancka <ana.polancec@gmail.com>
2023-11-17 21:52:52 +0100

    Lab2 finished, lab 3 partially finsihed

--- vaja1.py
+++ vaja1.py
@@ -14,17 +14,29 @@ import statsmodels.api as sm
 from statsmodels.graphics.gofplots import ProbPlot
 import math
 
-plt.style.use('seaborn') # pretty matplotlib plots
-plt.rc('font', size=14)
-plt.rc('figure', titlesize=18)
-plt.rc('axes', labelsize=15)
-plt.rc('axes', titlesize=18)
+#some call functions and plots are commented out because of time consuption
 
+#implementatioin of the linear regression fitting and predicting: 
+
+def my_linear_regression(X, Y):
+        X = np.array(X)
+        Y = np.array(Y)
+        #add the column of ones to calculate the intercept as well
+        X = np.c_[np.ones(X.shape[0]), X]
+        # Calculate the coefficients using the normal equation
+        beta = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(Y)
+        return beta
+
+def my_predict(X, beta):
+        X = np.c_[np.ones(X.shape[0]), X]
+        # Make predictions using the calculated coefficients
+        return X.dot(beta)
+
+
+##DATA PREPARATION
 
 #Download the dataset Auto MPG and remove the attribute cylinders and carname.
 data = pd.read_csv('data/auto-mpg.csv').drop(columns= 'car name').drop(columns= 'cylinders')
-
-# pay attention to missing values (horsepower!) and categorical attributes 
 
 # Data Sampling (shuffling the DataFrame)
 data = data.sample(frac=1, random_state=0)
@@ -35,7 +47,7 @@ data['horsepower'] = pd.to_numeric(data['horsepower'], errors='coerce', downcast
 # Removing Null Values
 data = data.dropna()
 
-#change the categorical value to dummie value
+#change the categorical value to dummie value --> this provided with worse results
 #data = pd.get_dummies(data, columns=['origin'])
 
 #Split your dataset on train and test data.
@@ -54,20 +66,20 @@ y = data[['mpg']]
 
 train_x, test_x, train_y, test_y = train_test_split(x, y, test_size= 0.3, random_state= 42)
 
+##LINEAR REGRESSION
 
 #Test your linear regression on the Auto dataset and compare it with the liear regression implemented in SciKit-learn.
 ofc_reg = LinearRegression().fit(train_x, train_y)
-model = myLinearRegression()
-betas = model.my_linear_regression(train_x, train_y)
+betas = my_linear_regression(train_x, train_y)
+
 
 # print(ofc_reg.intercept_)
 # print(ofc_reg.coef_)
 # print(betas)
 
-
-#comparing on r2 score
+#comparing on r2 score (they both yield the same svore)
 scikit_score = r2_score(test_y, ofc_reg.predict(test_x))
-my_score = r2_score(test_y, model.my_predict(test_x, betas))
+my_score = r2_score(test_y, my_predict(test_x, betas))
 
 #print(scikit_score)
 #print(my_score)
@@ -75,12 +87,11 @@ my_score = r2_score(test_y, model.my_predict(test_x, betas))
 
 #Diagnose your linear regression with the diagnostic plots.
 
-
 # Residuals vs. Fitted Values Plot:This plot displays the residuals (the differences between observed and predicted values) on the vertical axis and the fitted 
 # values (the predicted values) on the horizontal axis. It helps you check for linearity, homoscedasticity, and outliers.
 
 #get fitted values and residuals
-fitted_values = model.my_predict(test_x,betas)
+fitted_values = my_predict(test_x,betas)
 residuals = []
 print(test_y.iloc[0][0])
 
@@ -110,9 +121,9 @@ sm.qqplot(np.asarray(residuals), line='45')
 plt.title('Normal Q-Q Plot')
 plt.show()
 
-##COOKS DISTANCE
+##COOKS DISTANCE - checks for outliers with big leverage. 
 
-##correlation between attributes
+##correlation between attributes - there are some correlations, we do not like that
 correlation_matrix = data.corr()
 
 # Creating a heatmap of the correlation matrix
@@ -122,17 +133,10 @@ plt.title('Correlation Matrix of Features and Target')
 plt.show()
 
 
-## 
-attrs = ['displacement', 'horsepower', 'weight', 'acceleration', 'model year']
-fig, ax = plt.subplots(5,5)
-for i, attr1 in enumerate(attrs):
-    for j, attr2 in enumerate(attrs):
-        ax[i, j].set_xticks([])
-        ax[i, j].set_yticks([])
-        ax[i, j].scatter(data[attr1], data[attr2], s=0.2)
 
-plt.show()
 #Is there something you can improve like transform your target variable or remove
-#some attributes/instances? Try different things and refit the linear regression.
+#some attributes/instances? 
+
+# Origin variable could be hotcoded (tried with it, results were worse)
 
 
--- vaja3.py
+++ vaja3.py
@@ -10,37 +10,109 @@ from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 from sklearn.linear_model import Ridge
+from numpy import arange
+from sklearn.linear_model import RidgeCV
+from sklearn.model_selection import RepeatedKFold
+from sklearn.metrics import r2_score
+from sklearn.linear_model import Lasso
 
-
-def Ridge_function(x, y):
+##with ridge we get a lot less variance for small amount of added bias
+def Ridge_function(trainX, testX, trainY, testY):
     '''
     Fit model using ridge and tries different regularization parameter for evaluating its effect
     '''
-    n_alphas = 100
-    alphas = np.logspace(-10, -2, n_alphas)
+    n_alphas = 500
+    alphas = np.linspace(0, 10, n_alphas)
+    max_r = 0
+    max_alpha = 0
 
-    coefs = []
+    scores = list()
     for a in alphas:
         ridge = Ridge(alpha=a, fit_intercept=False)
-        ridge.fit(x, y)
-        coefs.append(ridge.coef_)
+        ridge.fit(trainX, trainY)
+        curr_r_score = r2_score(testY, ridge.predict(testX))
+        scores.append(curr_r_score)
+        if curr_r_score > max_r:
+            max_r = curr_r_score
+            max_alpha = a
 
-    ax = plt.gca()
-
-    #print(coefs)
-    ax.plot(alphas, coefs)
-    #ax.set_xscale("log")
-    ax.set_xlim(ax.get_xlim()[::-1])  # reverse axis
-    plt.xlabel("alpha")
-    plt.ylabel("weights")
-    plt.title("Ridge coefficients as a function of the regularization")
-    plt.axis("tight")
+    #print(max_alpha) # 2.224448897795591
+    plt.plot(alphas, scores)
+    plt.xlabel("Alpha value")
+    plt.ylabel("R^2 score")
+    plt.title("Ridge regression")
+    plt.axvline(x = max_alpha, color = 'red', linestyle = '--') # 2.224448897795591
     plt.show()
 
+def Lasso_function(trainX, testX, trainY, testY):
+    '''
+    Fit model using ridge and tries different regularization parameter for evaluating its effect
+    '''
+    n_alphas = 500
+    alphas = np.linspace(0.01, 10, n_alphas)
+    lasso = Lasso(max_iter=1000)
+    max_r = 0
+    max_alpha = 0
+    scores = []
+    rs = list()
+
+    for a in alphas:
+        lasso.set_params(alpha = a)
+        lasso.fit(trainX, trainY)
+        print(lasso.get_params())
+        curr_r_score = mean_squared_error(testY, lasso.predict(testX))
+        rs.append(curr_r_score)
+        if curr_r_score > max_r:
+             max_r = curr_r_score
+             max_alpha = a
+        scores.append(lasso.coef_)
+
+    print(max_alpha)
+    # plt.plot(alphas, scores)
+    # plt.xlabel("Alpha value")
+    # plt.ylabel("R^2 score")
+    # plt.title("Lasso regression")
+    # plt.axvline(x = max_alpha, color = 'red', linestyle = '--') #max alpha = 10
+    # plt.show()
+
+    # ax = plt.gca()
+    # ax.plot(alphas, scores)
+    # ax.set_xscale('log')
+    # plt.axis('tight')
+    # plt.xlabel('alpha')
+    # plt.ylabel('Standardized Coefficients')
+    # plt.title('Lasso coefficients as a function of alpha')
+    # plt.show()
+
+def my_Ridge_DG_fit(X, Y, learning_rate, iterations, penality):
+     # no_of_training_examples, no_of_features         
+        m, n = X.shape 
+          
+        # weight initialization         
+        W = np.zeros(n, dtype='int64').reshape(-1,1)
+        b = 0        
+
+        # gradient descent learning 
+                  
+        for i in range( iterations ) :             
+            #update the weights
+            Y_pred = my_Ridge_DG_predict(X, W, b) 
+            #print(Y_pred)
+            
+          
+        # calculate gradients       
+            dW = ( - ( 2 * (X.T).dot(Y - Y_pred ) ) + ( 2 * penality * W ) ) / m     
+            #print(dW) 
+            db = - 2 * np.sum(Y - Y_pred ) / m  
+            
+            # update weights     
+            W = W - learning_rate * dW     
+            b = b - learning_rate * db                   
+        return W,b
 
 
-
-
+def my_Ridge_DG_predict(X, W, b):
+    return X.dot(W) + b 
 
 
 
@@ -48,7 +120,7 @@ def Ridge_function(x, y):
 #Download the "Communities and Crime" dataset and prepare the data so that you will be able to use them for linear regression.
 
 # Reading the CSV file
-data = pd.read_csv('data/communities+and+crime/communities.data', na_values='?')
+data = pd.read_csv('communities.data', na_values='?')
 
 # Data Manipulation
 data = data.drop(['state', 'county', 'community', 'communityname', 'fold'], axis=1)
@@ -63,5 +135,58 @@ data = data.dropna(axis = 0)
 x  = data.drop(columns= 'ViolentCrimesPerPop')
 y = data[['ViolentCrimesPerPop']]
 
+trainX, testX, trainY, testY = train_test_split(x, y, test_size= 0.3, random_state= 42)
 
-Ridge_function(x, y)
+## Fit models using ridge and lasso regression.
+## Try different values of the regularization parameter and evaluate its effect. 
+
+#Ridge_function(trainX, testX, trainY, testY)
+#Lasso_function(trainX, testX, trainY, testY)
+
+# changing the alpha changes the slope of the predicted regression line. 
+# Alpha is optimal alpha is around 2,22 with ridge and 0.05 with lasso. 
+
+
+## Compare the results of the feature selection you implemented in the previous assignment
+## with the attributes lasso selected. - ##features are similar
+
+
+# Download the "Wine quality" dataset. Choose only the white wine data. Prepare
+# your data for modeling.
+
+dataWine = pd.read_csv('winequality-white.csv', na_values='?', sep=';')
+#print(dataWine.info) - no null values, no qualitative attributes
+
+X = dataWine.drop(columns= "quality").values
+Y = dataWine[['quality']].values
+
+
+X_train_w, X_test_w, Y_train_w, Y_test_w = train_test_split( X, Y, test_size = 0.3, random_state = 42) 
+
+# Implement ridge regression with:
+# • gradient descent
+
+iterations = 100
+learning_rate = 0.01
+penalty = 1
+mses = list()
+
+for i in range(iterations):
+    W,b = my_Ridge_DG_fit(X_train_w, Y_train_w, learning_rate, iterations, penalty)
+    ridgeDG_predictions = my_Ridge_DG_predict(X_test_w,W,b)
+    
+
+# print( "Predicted values ", np.round( ridgeDG_predictions[:3], 2 ) )      
+# print( "Real values      ", ridgeDG_predictions[:3] )
+# print( "Trained W        ",  round(W[0][0],2))     
+# print( "Trained b        ", round(b,2) )   
+
+# Trained W         -1.9193278495634436e+262
+# Trained b         -2.788072701411143e+261
+    
+# Visualization on test set      
+# plt.scatter( X_test_w[:,0], Y_test_w, color = 'blue' )     
+# plt.plot( X_test_w, ridgeDG_predictions, color = 'orange' )     
+# plt.show() 
+
+
