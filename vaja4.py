494a32f9490fdca679676fe56c5b09eaecce1ab4
polancka <ana.polancec@gmail.com>
2023-12-07 17:30:09 +0100

    Finsihed lab6, started lab4 (regression tree) - not working yet

--- vaja4.py
+++ vaja4.py
@@ -5,25 +5,94 @@ from sklearn.model_selection import cross_val_score
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
-# lahko droppaš variablo ki ima 4 različne (water body)
-
-class DecisionNode:
-    def __init__(self, right, left, rss_value, split_attribute):
-        self.left = left
-        self.right = right
-        self.rss_value = rss_value
-        self.split_attribute = split_attribute
-
-
-#Implement the regression tree algorithm from scratch. (split on a criteria of your own choosing)
-def build_regression_tree(trainX, trainY): #save the RSS value on every split and plot them to determine optimal value for stopping the tree
-    root_node = DecisionNode(None, None, 0, "")
-    return root_node
 
 #Select a criteria of your choice to stop splitting the nodes.
 # Selected split criteria is RSS (residual sum squared), which is most commonly used for regression. 
 #Criteria for ending the tree building can be depth of tree, minimal number of examples in a leaf node or a certain RSS value. 
+
+class MyRegressionTree:
+    def __init__(self, max_depth=None):
+        self.max_depth = max_depth
+
+    def fit(self, X, y, depth=0):
+        X = np.array(X)  # Convert X to numpy array
+        if depth == self.max_depth or len(np.unique(y)) == 1:
+            # If maximum depth or pure node, create a leaf node
+            return np.mean(y)
+
+        # Find the best split
+        split_index, split_value = self.find_best_split(X, y)
+
+        if split_index is None:
+            # If no split improves RSS, create a leaf node
+            return np.mean(y)
+
+        # Split the data
+        X_left, y_left, X_right, y_right = self.split_data(X, y, split_index, split_value)
+
+        # Recursively build the left and right subtrees
+        left_subtree = self.fit(X_left, y_left, depth + 1)
+        right_subtree = self.fit(X_right, y_right, depth + 1)
+
+        # Return a node representing the split
+        return {'split_index': split_index,
+                'split_value': split_value,
+                'left': left_subtree,
+                'right': right_subtree}
+
+    def find_best_split(self, X, y):
+        best_rss = float('inf')
+        best_split_index = None
+        best_split_value = None
+
+        for feature in range(X.shape[1]):
+            values = np.unique(X[:, feature])
+            for value in values:
+                X_left, y_left, X_right, y_right = self.split_data(X, y, feature, value)
+                rss = self.calculate_rss(y_left) + self.calculate_rss(y_right)
+
+                if rss < best_rss:
+                    best_rss = rss
+                    best_split_index = feature
+                    best_split_value = value
+
+        return best_split_index, best_split_value
+
+    def split_data(self, X, y, feature_index, split_value):
+        left_mask = X[:, feature_index] <= split_value
+        right_mask = ~left_mask
+
+        X_left, y_left = X[left_mask], y[left_mask]
+        X_right, y_right = X[right_mask], y[right_mask]
+
+        return X_left, y_left, X_right, y_right
+
+    def calculate_rss(self, y):
+        return np.sum((y - np.mean(y))**2)
+
+    def predict_instance(self, instance, tree):
+        if 'split_index' not in tree:
+            return tree  # leaf node
+
+        split_value = tree['split_value']
+        feature_value = instance[tree['split_index']]
+
+        if np.issubdtype(type(split_value), np.number) and np.issubdtype(type(feature_value), np.number):
+            # Check if both values are numeric before making the comparison
+            if feature_value <= split_value:
+                return self.predict_instance(instance, tree['left'])
+            else:
+                return self.predict_instance(instance, tree['right'])
+        else:
+            # If not numeric, treat as leaf node
+            return tree
+
+    def predict(self, X, tree):
+        return np.array([self.predict_instance(instance, tree) for instance in X])
+
+##---------------------------------------------------------------------------------------------------------------------------------
 
 #Download the dataset "House price". Price is the target.
 data = pd.read_csv("House_Price.csv")
@@ -32,23 +101,28 @@ X['airport'].replace(["NO", "YES"], #change the alchocol level (categorical vari
                         [0, 1], inplace=True)
 X['bus_ter'].replace(["NO", "YES"], #change the alchocol level (categorical variable) to a numeric variabče
                         [0, 1], inplace=True)
-print(X.info)
 Y = data['price']
 
-#TODO: prepare the data - what to do with categorical variables? Shoulkd the tree be build only on train set?
-
+#spliting the data
 trainX, testX, trainY, testY = train_test_split(X, Y, test_size= 0.3, random_state= 42)
+
 #Build a regression tree for the selected dataset.
-root_tree = build_regression_tree(trainX, trainY)
-my_score = cross_val_score(root_tree, testX, testY, cv = 10)
+max_depth = 7
+my_tree = MyRegressionTree(max_depth=max_depth)
+
+# Train the regression tree
+tree = my_tree.fit(trainX, trainY)
+
+# Make predictions
+y_pred = my_tree.predict(testX, tree)
 
 
 #Build a regression tree with scikit-learn
 regressor = DecisionTreeRegressor(trainX, trainX, random_state=0)
-sc_score = cross_val_score(regressor, testX, testY, cv=10)
-
-#TODO: Test the regressing tree (both) using cross-validation (save results as you go) 
 
 
-# TODO: Compare the cross-validation results with those you get while building a regression
+#  Compare the cross-validation results with those you get while building a regression
 #tree with scikit-learn. Use the same cross-validation splits on both models.
+#compare scores
+my_score = cross_val_score(my_tree, testX, testY, cv = 10)
+sc_score = cross_val_score(regressor, testX, testY, cv=10)
--- vaja6.py
+++ vaja6.py
@@ -10,72 +10,72 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-# #import and preprocess the breast-cancer data
-# data = pd.read_csv("bc.csv")
+#import and preprocess the breast-cancer data
+data = pd.read_csv("bc.csv")
 
-# data['diagnosis'].replace(["B", "M"], #change the diagnosis to a numeric variable
-#                         [0, 1], inplace=True)
+data['diagnosis'].replace(["B", "M"], #change the diagnosis to a numeric variable
+                        [0, 1], inplace=True)
 
-# data = data.drop(columns= 'Unnamed: 32')
+data = data.drop(columns= 'Unnamed: 32')
 
-# dataX = data.drop(columns= "diagnosis")
-# dataY = data["diagnosis"]
+dataX = data.drop(columns= "diagnosis")
+dataY = data["diagnosis"]
 
-## standardize the data
-# scaler = preprocessing.StandardScaler().fit(dataX)
-# dataX_scaled = scaler.transform(dataX)
-
-
-# print("data imported")
-
-# #split into training and testing data 
-# X_train, X_test, y_train, y_test = train_test_split(dataX_scaled, dataY, test_size=0.3,random_state=109) # 70% training and 30% test
-# print("data split")
-# #standardize the data
+# standardize the data
+scaler = preprocessing.StandardScaler().fit(dataX)
+dataX_scaled = scaler.transform(dataX)
 
 
-# #generate the model 
+print("data imported")
 
-# #Create a svm Classifier
-# clf = SVC(kernel='linear', C=0.5) # Linear Kernel (try different kernels - linear, polynominal (set the degree), radial basis)
-# print("Model made")
-# #Train the model using the training sets
-# clf.fit(X_train, y_train)
-# print("model fit")
-# #Predict the response for test dataset
-# y_pred = clf.predict(X_test)
-# print("predictions")
-# print(y_pred)
-
-# #Evaluate the model 
-# # Model Accuracy: how often is the classifier correct?
-# print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
-
-# # Model Precision: what percentage of positive tuples are labeled as such?
-# print("Precision:",metrics.precision_score(y_test, y_pred))
-
-# # Model Recall: what percentage of positive tuples are labelled as such?
-# print("Recall:",metrics.recall_score(y_test, y_pred))
+#split into training and testing data 
+X_train, X_test, y_train, y_test = train_test_split(dataX_scaled, dataY, test_size=0.3,random_state=109) # 70% training and 30% test
+print("data split")
+#standardize the data
 
 
+#generate the model 
 
-# # # TWEAK THE PARAMETERS : KERNEL, REGULARIZATION (C), GAMMA - WHICH VALUES??
-# # # Kernel: The main function of the kernel is to transform the given dataset input data into the required form. There are various types of functions such as linear, polynomial, and radial basis function (RBF). Polynomial and RBF are useful for non-linear hyperplane. Polynomial and RBF kernels compute the separation line in the higher dimension. In some of the applications, it is suggested to use a more complex kernel to separate the classes that are curved or nonlinear. This transformation can lead to more accurate classifiers.
-# # # Regularization: Regularization parameter in python's Scikit-learn C parameter used to maintain regularization. Here C is the penalty parameter, which represents misclassification or error term. The misclassification or error term tells the SVM optimization how much error is bearable. This is how you can control the trade-off between decision boundary and misclassification term. A smaller value of C creates a small-margin hyperplane and a larger value of C creates a larger-margin hyperplane.
-# # # Gamma: A lower value of Gamma will loosely fit the training dataset, whereas a higher value of gamma will exactly fit the training dataset, which causes over-fitting. In other words, you can say a low value of gamma considers only nearby points in calculating the separation line, while the a value of gamma considers all the data points in the calculation of the separation line.
-# param_grid = [
-#   {'C': [1, 10, 100, 1000], 'kernel': ['linear']},
-#   {'C': [1, 10, 100, 1000],'gamma': [0.001, 0.0001], 'kernel': ['rbf']},
-#   {'C': [1, 10, 100, 1000], 'degree': [2,3,4,5,6,7,8,9,10], 'kernel': ['poly']},
-#  ]
-# grid_search = GridSearchCV(clf,param_grid)
-# grid_search.fit(X_train, y_train)
+#Create a svm Classifier
+clf = SVC(kernel='linear', C=0.5) # Linear Kernel (try different kernels - linear, polynominal (set the degree), radial basis)
+print("Model made")
+#Train the model using the training sets
+clf.fit(X_train, y_train)
+print("model fit")
+#Predict the response for test dataset
+y_pred = clf.predict(X_test)
+print("predictions")
+print(y_pred)
 
-# # print best parameter after tuning 
-# print(grid_search.best_params_) 
+#Evaluate the model 
+# Model Accuracy: how often is the classifier correct?
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+
+# Model Precision: what percentage of positive tuples are labeled as such?
+print("Precision:",metrics.precision_score(y_test, y_pred))
+
+# Model Recall: what percentage of positive tuples are labelled as such?
+print("Recall:",metrics.recall_score(y_test, y_pred))
+
+
+
+# # TWEAK THE PARAMETERS : KERNEL, REGULARIZATION (C), GAMMA - WHICH VALUES??
+# # Kernel: The main function of the kernel is to transform the given dataset input data into the required form. There are various types of functions such as linear, polynomial, and radial basis function (RBF). Polynomial and RBF are useful for non-linear hyperplane. Polynomial and RBF kernels compute the separation line in the higher dimension. In some of the applications, it is suggested to use a more complex kernel to separate the classes that are curved or nonlinear. This transformation can lead to more accurate classifiers.
+# # Regularization: Regularization parameter in python's Scikit-learn C parameter used to maintain regularization. Here C is the penalty parameter, which represents misclassification or error term. The misclassification or error term tells the SVM optimization how much error is bearable. This is how you can control the trade-off between decision boundary and misclassification term. A smaller value of C creates a small-margin hyperplane and a larger value of C creates a larger-margin hyperplane.
+# # Gamma: A lower value of Gamma will loosely fit the training dataset, whereas a higher value of gamma will exactly fit the training dataset, which causes over-fitting. In other words, you can say a low value of gamma considers only nearby points in calculating the separation line, while the a value of gamma considers all the data points in the calculation of the separation line.
+param_grid = [
+  {'C': [1, 10, 100, 1000], 'kernel': ['linear']},
+  {'C': [1, 10, 100, 1000],'gamma': [0.001, 0.0001], 'kernel': ['rbf']},
+  {'C': [1, 10, 100, 1000], 'degree': [2,3,4,5,6,7,8,9,10], 'kernel': ['poly']},
+ ]
+grid_search = GridSearchCV(clf,param_grid)
+grid_search.fit(X_train, y_train)
+
+# print best parameter after tuning 
+print(grid_search.best_params_) 
   
-# # print how our model looks after hyper-parameter tuning 
-# print(grid_search.best_estimator_) 
+# print how our model looks after hyper-parameter tuning 
+print(grid_search.best_estimator_) 
 
 
 #--------------------------------------------------------------2nd part -----------------------------------------------------------------------------------------#
@@ -86,7 +86,7 @@ def linear_kernel(x1, x2):
 def polynomial_kernel(x1, x2, degree=3):
     return (np.dot(x1, x2) + 3) ** degree
 
-def radial_basis_kernel(x1, x2, gamma=1.0):
+def radial_basis_kernel(x1, x2, gamma=5.0):
     return np.exp(-gamma * np.linalg.norm(x1 - x2) ** 2)
 
 #kernel matrix 
@@ -98,8 +98,6 @@ def kernel_matrix(X, kernel_func):
             K[i, j] = kernel_func(X[i], X[j])
     return K
 
-#the linear might not work! try other two
-
 ## MY KERNEL REGRESSION
 def kernel_regression(trainX, trainY, testX, reg_lambda=1e-4): 
     K_linear = kernel_matrix(trainX, linear_kernel)
@@ -109,24 +107,31 @@ def kernel_regression(trainX, trainY, testX, reg_lambda=1e-4):
     alpha_linear = np.linalg.inv(K_linear + reg_lambda * np.identity(len(trainX))) @ trainY 
     alpha_poly = np.linalg.inv(K_poly  + reg_lambda * np.identity(len(trainX))) @ trainY
     alpha_radial = np.linalg.inv(K_radial  + reg_lambda * np.identity(len(trainX))) @ trainY
+
+    linear_predictions = np.zeros(len(testX))
+    for i in range(len(testX)):
+        kernel_vec = np.zeros(len(trainX))
+        for j in range(len(trainX)):
+            kernel_vec[j] = linear_kernel(testX[i], trainX[j])
+        linear_predictions[i] = np.dot(kernel_vec, alpha_linear)
     
-    #y = k(testx) * alpha
-    #TODO: popravi način izračuna predictionov! zdaj ne dela
 
-    dot_prods = list()
-    for i in range(len(testX)): 
-        for j in range(len(trainX)): 
-            current_prod = linear_kernel(testX[i], trainX[j])
-            dot_prods.append(current_prod)
-            #print(dot_prods)
+    poly_predictions = np.zeros(len(testX))
+    for i in range(len(testX)):
+        kernel_vec = np.zeros(len(trainX))
+        for j in range(len(trainX)):
+            kernel_vec[j] = polynomial_kernel(testX[i], trainX[j])
+        poly_predictions[i] = np.dot(kernel_vec, alpha_poly)
+    
 
-    lin_pred =  np.array(dot_prods) @ alpha_linear
-    print(lin_pred)
-
-    poly_predictions = np.array([polynomial_kernel(testX, x) for x in trainX]) @ alpha_poly
-    radial_predictions = np.array([radial_basis_kernel(testX, x) for x in trainX]) @ alpha_radial
-
-    return lin_pred, poly_predictions, radial_predictions
+    radial_predictions = np.zeros(len(testX))
+    for i in range(len(testX)):
+        kernel_vec = np.zeros(len(trainX))
+        for j in range(len(trainX)):
+            kernel_vec[j] = radial_basis_kernel(testX[i], trainX[j])
+        linear_predictions[i] = np.dot(kernel_vec, alpha_radial)
+    
+    return linear_predictions, poly_predictions, radial_predictions
 
 
 #import data for fitting (#2nd part) 
@@ -141,36 +146,27 @@ xToFit_scaled = scaler.transform(xToFit)
 Xtrain2, Xtest2, ytrain2, ytest2 = train_test_split(xToFit_scaled, yToFit, test_size=0.3,random_state=109) # 70% training and 30% test
 #X_test_points = np.linspace(min(Xtest2), max(Xtest2), 500).reshape(-1, 1)
 
-
-#checking for correlation of the data because of getting a singular matrix in the kernel! -> High correlations between some attributes
-#correlation_matrix = np.corrcoef(X_train, rowvar=False)
-#print(correlation_matrix)
-# plt.figure(figsize=(10, 8))
-# sns.heatmap(correlation_matrix, annot=False, cmap='coolwarm', fmt=".2f", linewidths=0.5)
-# plt.title('Correlation Matrix')
-# plt.show()
-
 lin_pred, poly_pred, rad_pred = kernel_regression(Xtrain2, ytrain2, Xtest2)
 
 # Plot the data and predictions for each kernel
 plt.figure(figsize=(15, 5))
 
-plt.subplot(1, 3, 0)
-plt.scatter(Xtrain2, ytrain2, label='Data')
-plt.plot(Xtest2, lin_pred, color='red', label='Predictions')
-plt.title(f'Kernel linear')
+plt.subplot(1, 3, 1)
+plt.scatter(Xtest2, ytest2, label='Data', color="blue")
+plt.scatter(Xtest2, lin_pred, color='red', label='Predictions')
+plt.title(f'Linear kernel')
 plt.legend()
 
-plt.subplot(1, 3, 1)
-plt.scatter(Xtrain2, ytrain2, label='Data')
-plt.plot(Xtest2, poly_pred, color='red', label='Predictions')
-plt.title(f'Kernel polynomial')
+plt.subplot(1, 3, 2)
+plt.scatter(Xtest2, ytest2, label='Data', color="blue")
+plt.scatter(Xtest2, poly_pred, color='red', label='Predictions')
+plt.title(f'Polynomial kernel')
 plt.legend()
 
-plt.subplot(1, 3, 1)
-plt.scatter(Xtrain2, ytrain2, label='Data')
-plt.plot(Xtest2, rad_pred,  color='red', label='Predictions')
-plt.title(f'Kernel radial basis')
+plt.subplot(1, 3, 3)
+plt.scatter(Xtest2, ytest2, label='Data', color="blue")
+plt.scatter(Xtest2, rad_pred, color='red', label='Predictions')
+plt.title(f'Radial kernel')
 plt.legend()
 
 plt.show()
